692f803a6789f0f7944b1fb144ca285afa159d18
Fran <fran.siner@gmail.com>
2022-12-11 16:04:33 +0100

    Added sitemap, robots.txt and some lighthouse fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import mdx from '@astrojs/mdx'
 
-import mdx from "@astrojs/mdx";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()]
-});
+  site: 'https://franmoreno.com',
+  integrations: [tailwind(), react(), mdx(), sitemap()]
+})
--- src/components/elements/switchDark.jsx
+++ src/components/elements/switchDark.jsx
@@ -1,46 +1,47 @@
-import React, { useState, useEffect } from "react";
-import Moon from "./moon";
-import Sun from "./sun";
+import React, { useState, useEffect } from 'react'
+import Moon from './moon'
+import Sun from './sun'
 
 const updateTheme = (isDarkEnabled) => {
   // Get CSS variables for background/foreground
-  const styles = getComputedStyle(document.body);
-  const black = styles.getPropertyValue("--black");
-  const white = styles.getPropertyValue("--white");
-  const docEl = document.documentElement;
+  const styles = getComputedStyle(document.body)
+  const black = styles.getPropertyValue('--black')
+  const white = styles.getPropertyValue('--white')
+  const docEl = document.documentElement
 
   if (isDarkEnabled) {
-    docEl.style.setProperty("--background", black);
-    docEl.style.setProperty("--foreground", white);
-    document.querySelector("html").classList.add("dark");
+    docEl.style.setProperty('--background', black)
+    docEl.style.setProperty('--foreground', white)
+    document.querySelector('html').classList.add('dark')
   } else {
-    docEl.style.setProperty("--background", white);
-    docEl.style.setProperty("--foreground", black);
-    document.querySelector("html").classList.remove("dark");
+    docEl.style.setProperty('--background', white)
+    docEl.style.setProperty('--foreground', black)
+    document.querySelector('html').classList.remove('dark')
   }
-};
+}
 
 function SwitchDark() {
-  const [isEnabled, setIsEnabled] = useState(false);
+  const [isEnabled, setIsEnabled] = useState(false)
 
   useEffect(() => {
-    updateTheme(isEnabled);
-  }, [isEnabled]);
+    updateTheme(isEnabled)
+  }, [isEnabled])
 
   const toggleState = () => {
-    setIsEnabled((prevState) => !prevState);
-  };
+    setIsEnabled((prevState) => !prevState)
+  }
 
   return (
     <label className="toggle-wrapper" htmlFor="toggle">
-      <div className={`toggle ${isEnabled ? "enabled" : "disabled"}`}>
+      <div className={`toggle ${isEnabled ? 'enabled' : 'disabled'}`}>
         <span className="hidden">
-          {isEnabled ? "Enable Light Mode" : "Enable Dark Mode"}
+          {isEnabled ? 'Enable Light Mode' : 'Enable Dark Mode'}
         </span>
         <div className="icons">
           <Sun />
           <Moon />
         </div>
+        <label for="toggle" className="hidden"></label>
         <input
           id="toggle"
           name="toggle"
@@ -51,7 +52,7 @@ function SwitchDark() {
         />
       </div>
     </label>
-  );
+  )
 }
 
-export default SwitchDark;
+export default SwitchDark
