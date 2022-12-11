f38294eff8e4b932efbc82d063395458ff311976
Fran <fran.siner@gmail.com>
2022-12-11 20:37:32 +0100

    Changes in the dark mode

--- src/components/elements/switchDark.jsx
+++ src/components/elements/switchDark.jsx
@@ -2,52 +2,39 @@ import React, { useState, useEffect } from 'react'
 import Moon from './moon'
 import Sun from './sun'
 
-const updateTheme = (isDarkEnabled) => {
-  // Get CSS variables for background/foreground
-  const styles = getComputedStyle(document.body)
-  const black = styles.getPropertyValue('--black')
-  const white = styles.getPropertyValue('--white')
-  const docEl = document.documentElement
-
-  if (isDarkEnabled) {
-    docEl.style.setProperty('--background', black)
-    docEl.style.setProperty('--foreground', white)
-    document.querySelector('html').classList.add('dark')
-  } else {
-    docEl.style.setProperty('--background', white)
-    docEl.style.setProperty('--foreground', black)
-    document.querySelector('html').classList.remove('dark')
-  }
-}
-
 function SwitchDark() {
-  const [isEnabled, setIsEnabled] = useState(false)
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ?? 'light')
+
+  const handleClick = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
 
   useEffect(() => {
-    updateTheme(isEnabled)
-  }, [isEnabled])
-
-  const toggleState = () => {
-    setIsEnabled((prevState) => !prevState)
-  }
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+    localStorage.setItem('theme', theme)
+  }, [theme])
 
   return (
     <label className="toggle-wrapper" htmlFor="toggle">
-      <div className={`toggle ${isEnabled ? 'enabled' : 'disabled'}`}>
+      <div className={`toggle ${theme === 'dark' ? 'enabled' : 'disabled'}`}>
         <span className="hidden">
-          {isEnabled ? 'Enable Light Mode' : 'Enable Dark Mode'}
+          {theme === 'dark' ? 'Enable Light Mode' : 'Enable Dark Mode'}
         </span>
         <div className="icons">
           <Sun />
           <Moon />
         </div>
-        <label for="toggle" className="hidden"></label>
+        <label forhtml="toggle" className="hidden"></label>
         <input
           id="toggle"
           name="toggle"
           type="checkbox"
-          checked={isEnabled}
-          onClick={toggleState}
+          checked={theme === 'dark'}
+          onClick={handleClick}
           readOnly
         />
       </div>
