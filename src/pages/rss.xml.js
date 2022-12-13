e879f9df6015e48c3c3accf8abaae7e84b0ae8ac
Fran <fran.siner@gmail.com>
2022-12-13 12:21:58 +0100

    Changed rss to allow full content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,18 @@
 import rss from '@astrojs/rss'
+import sanitizeHtml from 'sanitize-html'
+const postImportResult = import.meta.glob('./blog/*.md', { eager: true })
+const posts = Object.values(postImportResult)
 
 export const get = () =>
   rss({
     title: 'Fran Moreno | Blog',
     description: 'El Blog de Fran Moreno',
     site: 'https://franmoreno.com',
-    items: import.meta.glob('./blog/*.mdx'),
+    items: posts.map((post) => ({
+      link: post.url,
+      title: post.frontmatter.title,
+      pubDate: post.frontmatter.pubDate,
+      content: sanitizeHtml(post.compiledContent())
+    })),
     customData: `<language>es-es</language>`
   })
