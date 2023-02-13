e58c68b7de5669df79729836f077b16857d36337
Fran <fran.siner@gmail.com>
2023-02-13 12:06:02 +0100

    Updating to Astro 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://franmoreno.com',
+  site: 'https://franmoreno.com/',
   integrations: [tailwind(), react(), mdx(), sitemap()]
 })
